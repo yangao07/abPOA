4214a056178e987f8c5db47cb6ec0c41ee9375b1
Yan Gao <yangao07@hit.edu.cn>
2020-05-26 23:53:32 +0800

    add racon msa

--- evaluation/msa_abPOA.c
+++ evaluation/msa_abPOA.c
@@ -10,8 +10,7 @@
 #include <getopt.h>
 #include "abpoa.h"
 
-#define MAX_SEQ_LEN 10000
-#define MAX_INDEL_LEN 1000
+#define MAX_SEQ_LEN 20000
 // AaCcGgTtNn ==> 0,1,2,3,4
 unsigned char nst_nt4_table[256] = {
 	4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4, 
@@ -31,6 +30,28 @@ unsigned char nst_nt4_table[256] = {
 	4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4, 
 	4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4
 };
+
+char *get_seq_for_racon(FILE *fp, int *start) {
+    char c = fgetc(fp);
+    if (feof(fp)) return NULL; // end of file
+    else if (c != '>') {
+        fprintf(stderr, "Unexpected format.\n");
+        exit(1);
+    }
+    // starts with '>'
+    char *seq = (char *)malloc(sizeof(char) * MAX_SEQ_LEN);
+    fgets(seq, MAX_SEQ_LEN, fp); // header line
+    if (seq[0] == '0') *start = 1;
+    else *start = 0;
+    if(fgets(seq, MAX_SEQ_LEN, fp) == NULL) { // sequence line
+        fprintf(stderr, "No fasta sequence.\n");
+        exit(1); 
+    }
+
+    int seq_len = strlen(seq) - 1;
+    seq[seq_len] = '\0';
+    return seq;
+}
 
 // read fasta format
 char *get_seq(FILE *fp)
@@ -81,6 +102,8 @@ void help() {
         "        -n <int>\n"
         "            default: 10\n"
         "            number of sequences in each set\n"
+        "        -r \n"
+        "            input sequence set is from racon\n"
         "        -h \n"
         "            prints the usage\n"
     );
@@ -92,8 +115,9 @@ int main (int argc, char * const argv[]) {
     abpoa_para_t *abpt = abpoa_init_para();
 
     int n_seqs = 0, *seq_lens; uint8_t **bseqs;
-    char opt; char *s;;
-    while ((opt = getopt(argc, argv, "l:m:x:o:e:s:n:hb:")) != -1) {
+    char opt; char *s;
+    int for_racon = 0;
+    while ((opt = getopt(argc, argv, "l:m:x:o:e:s:n:rhb:")) != -1) {
         switch (opt) {
             case 'l': abpt->align_mode = atoi(optarg); break;
             case 'm': abpt->match = atoi(optarg); break;
@@ -102,14 +126,13 @@ int main (int argc, char * const argv[]) {
             case 'e': abpt->gap_ext1 = strtol(optarg, &s, 10); if (*s == ',') abpt->gap_ext2 = strtol(s+1, &s, 10); break;
             case 's': seq_file = optarg; break;
             case 'n': n_seqs = atoi(optarg); break;
+            case 'r': for_racon = 1; break;
             case 'b': abpt->wb = atoi(optarg); break;
             case 'h': help(); return 0;
             default: help(); return 1;
         }
     }
 
-    seq_lens = (int*)malloc(sizeof(int) * n_seqs);
-    bseqs = (uint8_t**)malloc(sizeof(uint8_t*) * n_seqs);
 
     // output options
     abpt->out_cons = 1; // generate consensus sequence, set 0 to disable
@@ -123,37 +146,80 @@ int main (int argc, char * const argv[]) {
     }
 
     struct timeval start_time, end_time;
-    double runtime = 0;
-
-    int i,j = 0;
+    double runtime = 0; int i; char *seq;
     abpoa_t *ab = abpoa_init();
-    while(1) {
-        int seq_i = 0;
+    if (for_racon) {
+        n_seqs = 500;
+        seq_lens = (int*)malloc(sizeof(int) * n_seqs);
+        bseqs = (uint8_t**)malloc(sizeof(uint8_t*) * n_seqs);
+        int start, init=1, seq_i = 0;
+        while (1) {
+            while (1) {
+                seq = get_seq_for_racon(fp_seq, &start);
+                if (seq == NULL || start == 1) break;
+                int seq_len = strlen(seq);
+                seq_lens[seq_i] = seq_len;
+                bseqs[seq_i] = (uint8_t*)malloc(seq_len * sizeof(uint8_t));
+                for (i = 0; i < seq_len; ++i)
+                    bseqs[seq_i][i] = nst_nt4_table[(int)seq[i]];
+                free(seq); 
+                seq_i++;
+            }
+            if (init) init = 0;
+            else {
+                // do msa
+                // printf("%d\n", seq_i);
+                gettimeofday(&start_time, NULL);
+                abpoa_msa(ab, abpt, seq_i, seq_lens, bseqs, stdout, NULL, NULL, NULL, NULL, NULL);
+                gettimeofday(&end_time, NULL);
+                runtime = runtime + (end_time.tv_sec - start_time.tv_sec) + (end_time.tv_usec - start_time.tv_usec) * 1e-6;
+
+                if (seq_i > 0) {
+                    for (i = 0; i < seq_i; ++i) free(bseqs[i]); 
+                }
+                if(feof(fp_seq)) break;
+            }
+            if (start) {
+                int seq_len = strlen(seq);
+                seq_lens[0] = seq_len;
+                bseqs[0] = (uint8_t*)malloc(seq_len * sizeof(uint8_t));
+                for (i = 0; i < seq_len; ++i)
+                    bseqs[0][i] = nst_nt4_table[(int)seq[i]];
+                free(seq); 
+                seq_i = 1;
+            }
+        }
+    } else {
+        seq_lens = (int*)malloc(sizeof(int) * n_seqs);
+        bseqs = (uint8_t**)malloc(sizeof(uint8_t*) * n_seqs);
         while(1) {
-            char *seq = get_seq(fp_seq);
-            if(seq == NULL ) break;
+            int seq_i = 0;
+            while(1) {
+                seq = get_seq(fp_seq);
+                if(seq == NULL ) break;
 
-            int seq_len = strlen(seq);
-            seq_lens[seq_i] = seq_len;
-            bseqs[seq_i] = (uint8_t*)malloc(seq_len * sizeof(uint8_t));
-            for (i = 0; i < seq_len; ++i)
-                bseqs[seq_i][i] = nst_nt4_table[(int)seq[i]];
-            free(seq); 
-            seq_i++;
-            if (seq_i == n_seqs) break; // read a set of sequences
+                int seq_len = strlen(seq);
+                seq_lens[seq_i] = seq_len;
+                bseqs[seq_i] = (uint8_t*)malloc(seq_len * sizeof(uint8_t));
+                for (i = 0; i < seq_len; ++i)
+                    bseqs[seq_i][i] = nst_nt4_table[(int)seq[i]];
+                free(seq); 
+                seq_i++;
+                if (seq_i == n_seqs) break; // read a set of sequences
+            }
+            if (seq_i == 0) break;
+
+            gettimeofday(&start_time, NULL);
+            abpoa_msa(ab, abpt, n_seqs, seq_lens, bseqs, stdout, NULL, NULL, NULL, NULL, NULL);
+            gettimeofday(&end_time, NULL);
+            runtime = runtime + (end_time.tv_sec - start_time.tv_sec) + (end_time.tv_usec - start_time.tv_usec) * 1e-6;
+
+            if (n_seqs > 0) {
+                for (i = 0; i < n_seqs; ++i) free(bseqs[i]); 
+            }
+
+            if(feof(fp_seq)) break;
         }
-        if (seq_i == 0) break;
-
-        gettimeofday(&start_time, NULL);
-        abpoa_msa(ab, abpt, n_seqs, seq_lens, bseqs, stdout, NULL, NULL, NULL, NULL, NULL);
-        gettimeofday(&end_time, NULL);
-        runtime = runtime + (end_time.tv_sec - start_time.tv_sec) + (end_time.tv_usec - start_time.tv_usec) * 1e-6;
-
-        if (n_seqs > 0) {
-            for (i = 0; i < n_seqs; ++i) free(bseqs[i]); 
-        }
-
-        if(feof(fp_seq)) break;
     }
     abpoa_free(ab, abpt);
     fprintf(stderr, "%.2f ", runtime);
--- evaluation/msa_spoa.cpp
+++ evaluation/msa_spoa.cpp
@@ -35,6 +35,22 @@ string get_seq(ifstream& in_file)
     return seq;
 }
 
+string get_seq_for_racon(ifstream& in_file, bool *start)
+{
+    char c = in_file.get();
+    if (in_file.eof()) return "";
+    else if (c != '>') {
+        cout << "Unexpected format." << endl;
+        exit(1);
+    }
+    string seq;
+    getline(in_file, seq); // header line
+    if (seq[0] == '0') *start = true;
+    else *start = false;
+    getline(in_file, seq); // sequence line
+    return seq;
+}
+
 void help() {
     std::cout <<
         "\n"
@@ -59,6 +75,8 @@ void help() {
         "        -n <int>\n"
         "            default: 10\n"
         "            number of sequences in each set\n"
+        "        -r \n"
+        "            input sequence set is from racon\n"
         "        -h \n"
         "            prints the usage\n";
 }
@@ -74,8 +92,8 @@ int main(int argc, char** argv) {
     std::int8_t o2 = -24;
     std::int8_t e2 = -1;
 
-    char opt, *s; int n_seqs = 0;
-    while ((opt = getopt(argc, argv, "l:m:x:o:n:e:q:c:s:h")) != -1) {
+    char opt, *s; int n_seqs = 0, for_racon = 0;
+    while ((opt = getopt(argc, argv, "l:m:x:o:n:e:q:c:s:rh")) != -1) {
         switch (opt) {
             case 'm': m = atoi(optarg); break;
             case 'x': x = 0-atoi(optarg); break;
@@ -83,6 +101,7 @@ int main(int argc, char** argv) {
             case 'e': e1 = 0-strtol(optarg, &s, 10); if (*s == ',') e2 = 0-strtol(s+1, &s, 10); break;
             case 'n': n_seqs = atoi(optarg); break;
             case 's': seq_file = optarg; break;
+            case 'r': for_racon = 1; break;
             case 'h': help(); return 0;
             default: help(); return 1;
         }
@@ -93,7 +112,7 @@ int main(int argc, char** argv) {
     std::unique_ptr<spoa::AlignmentEngine> alignment_engine;
     try {
         alignment_engine = spoa::createAlignmentEngine(
-            static_cast<spoa::AlignmentType>(algorithm), m, x, oe1, e1, oe2, e2);
+                static_cast<spoa::AlignmentType>(algorithm), m, x, oe1, e1, oe2, e2);
     } catch(std::invalid_argument& exception) {
         std::cerr << exception.what() << std::endl;
         return 1;
@@ -104,34 +123,73 @@ int main(int argc, char** argv) {
     assert(fp_seq.is_open());
 
     struct timeval start_time, end_time;
-    double runtime = 0;
+    double runtime = 0; int seq_i; string seq;
 
-    while(1) {
-        auto graph = spoa::createGraph();
-
-        int seq_i = 0;
+    if (for_racon) {
+        seq_i = 0;
+        int init = 1; bool start;
         while(1) {
-            string seq = get_seq(fp_seq);
-            if(seq == "") break;
+            auto graph = spoa::createGraph();
+
+            while(1) {
+                seq = get_seq_for_racon(fp_seq, &start);
+                if(seq == "" || start) break;
+
+                gettimeofday(&start_time, NULL);
+                auto alignment = alignment_engine->align(seq, graph);
+                graph->add_alignment(alignment, seq);
+                gettimeofday(&end_time, NULL);
+                runtime += (end_time.tv_sec - start_time.tv_sec) + (end_time.tv_usec - start_time.tv_usec) * 1e-6;
+                seq_i++;
+            }
+            if (init) init = 0;
+            else {
+                // cout << seq_i << endl;
+
+                gettimeofday(&start_time, NULL);
+                std::string consensus = graph->generate_consensus();
+                std::cout << ">Consensus_sequence" << endl;
+                std::cout << consensus.c_str() << endl;
+                gettimeofday(&end_time, NULL);
+                runtime += (end_time.tv_sec - start_time.tv_sec) + (end_time.tv_usec - start_time.tv_usec) * 1e-6;
+                if (seq_i == 0 || fp_seq.eof()) break;
+            }
+            if (start) {
+                gettimeofday(&start_time, NULL);
+                auto alignment = alignment_engine->align(seq, graph);
+                graph->add_alignment(alignment, seq);
+                gettimeofday(&end_time, NULL);
+                runtime += (end_time.tv_sec - start_time.tv_sec) + (end_time.tv_usec - start_time.tv_usec) * 1e-6;
+                seq_i = 1;
+            }
+        }
+    } else {
+        while(1) {
+            auto graph = spoa::createGraph();
+
+            seq_i = 0;
+            while(1) {
+                seq = get_seq(fp_seq);
+                if(seq == "") break;
+
+                gettimeofday(&start_time, NULL);
+                auto alignment = alignment_engine->align(seq, graph);
+                graph->add_alignment(alignment, seq);
+                gettimeofday(&end_time, NULL);
+                runtime += (end_time.tv_sec - start_time.tv_sec) + (end_time.tv_usec - start_time.tv_usec) * 1e-6;
+
+                if (++seq_i == n_seqs) break;
+            }
+
+            if (seq_i == 0 || fp_seq.eof()) break;
 
             gettimeofday(&start_time, NULL);
-            auto alignment = alignment_engine->align(seq, graph);
-            graph->add_alignment(alignment, seq);
+            std::string consensus = graph->generate_consensus();
+            std::cout << ">Consensus_sequence" << endl;
+            std::cout << consensus.c_str() << endl;
             gettimeofday(&end_time, NULL);
             runtime += (end_time.tv_sec - start_time.tv_sec) + (end_time.tv_usec - start_time.tv_usec) * 1e-6;
-
-            if (++seq_i == n_seqs) break;
         }
-        
-        if (seq_i == 0 || fp_seq.eof()) break;
-
-        gettimeofday(&start_time, NULL);
-        std::string consensus = graph->generate_consensus();
-        std::cout << ">Consensus_sequence" << endl;
-        std::cout << consensus.c_str() << endl;
-        gettimeofday(&end_time, NULL);
-        runtime += (end_time.tv_sec - start_time.tv_sec) + (end_time.tv_usec - start_time.tv_usec) * 1e-6;
-
     }
     fprintf(stderr, "%.2f ", runtime);
 
