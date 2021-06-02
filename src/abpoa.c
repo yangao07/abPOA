bb4e3038da99074265f71e819388c493ad915db4
Yan Gao <gaoy286@mail.sysu.edu.cn>
2021-06-02 10:16:27 +0800

    v1.2.4

--- src/abpoa.c
+++ src/abpoa.c
@@ -16,7 +16,7 @@ char PROG[20] = "abpoa";
 #define _bO BOLD UNDERLINE "O" NONE
 #define _bA BOLD UNDERLINE "A" NONE
 char DESCRIPTION[100] = _ba "daptive " _bb "anded " _bP "artial " _bO "rder " _bA "lignment";
-char VERSION[20] = "1.2.3";
+char VERSION[20] = "1.2.4";
 char CONTACT[30] = "gaoy286@mail.sysu.edu.cn";
 
 const struct option abpoa_long_opt [] = {
